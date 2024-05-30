6874810c1ac8848b8f4c51444ba4b7eb09df9d61
sowon.na <sowon.na@samsung.com>
2024-05-30 11:51:31 +0900

    [feature] page 40

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -19,3 +19,8 @@ TEST_F(PrimeFixure, Of2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+
+TEST_F(PrimeFixure, Of3) {
+	expected = {3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
